fcbd04be139d607d9fdd26672b374db0cd2b1a33
Leandro <souzaleandro79@gmail.com>
2022-06-25 20:43:01 -0300

    Validação de campo obrigatório

--- src/test/java/com/thecatapi/TestApiCat.java
+++ src/test/java/com/thecatapi/TestApiCat.java
@@ -1,6 +1,7 @@
 package com.thecatapi;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class TestApiCat {
 
@@ -12,9 +13,27 @@ public class TestApiCat {
         .when()
                 .post(" https://api.thecatapi.com/v1/user/passwordlesssignup\n")
         .then()
+                .statusCode(200)
                  .log().all();
     }
+
+    @Test
+    public void cadastroApiCampoObrigatorio(){
+        given()
+                .contentType("application/json")
+                .body("{\"appDescription\": \"teste 123\"}")
+        .when()
+                .post(" https://api.thecatapi.com/v1/user/passwordlesssignup\n")
+        .then()
+                .log().all()
+                .body("message",containsString("\"email\" is required"))
+                .statusCode(400);
+    }
+
+
+
 }
+
 
 
 
