d1ee0b2861228562995106859042872b2b41cb4f
Leandro <souzaleandro79@gmail.com>
2022-06-25 21:27:15 -0300

    Test efetuando votação

--- src/test/java/com/thecatapi/TestApiCat.java
+++ src/test/java/com/thecatapi/TestApiCat.java
@@ -6,7 +6,7 @@ import static org.hamcrest.Matchers.*;
 public class TestApiCat {
 
     @Test
-    public void cadastroApi(){
+    public void testCadastroApi(){
         given()
                 .contentType("application/json")
                 .body("{\"email\": \"une@gmail.com\", \"appDescription\": \"teste 123\"}")
@@ -18,7 +18,7 @@ public class TestApiCat {
     }
 
     @Test
-    public void cadastroApiCampoObrigatorio(){
+    public void testCadastroApiCampoObrigatorio(){
         given()
                 .contentType("application/json")
                 .body("{\"appDescription\": \"teste 123\"}")
@@ -28,6 +28,46 @@ public class TestApiCat {
                 .log().all()
                 .body("message",containsString("\"email\" is required"))
                 .statusCode(400);
+    }
+// 0455fc37-94cb-4b00-8760-3837c3cdecf3
+    @Test
+    public void testEfetuarVotacao(){
+        given()
+                .contentType("application/json")
+                .body("  {\"image_id\": \"cjc\", \"value\": true, \"sub_id\": \"demo-a4ccbf\"}")
+                .header("x-api-key","0455fc37-94cb-4b00-8760-3837c3cdecf3")
+        .when()
+                .post("https://api.thecatapi.com/v1/votes/")
+        .then()
+                .statusCode(200)
+                .body("message", is("SUCCESS"))
+                .log().all();
+    }
+
+
+
+    @Test
+    public void testPegarVotacao(){
+        given()
+                .contentType("application/json")
+                .body("")
+        .when()
+                .post(" ")
+        .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void testDeletarVotacao(){
+        given()
+                .contentType("application/json")
+                .body("")
+        .when()
+                .post(" ")
+        .then()
+                .statusCode(200)
+                .log().all();
     }
 
 
